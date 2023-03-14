2672f0f4b82655f5d7c5caca537815efc0fd84f9
maninhomarcos <111518436+maninhomarcos@users.noreply.github.com>
2023-03-14 12:15:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>text.php</title>
+    <link rel="styleshit"href=style.css>
+
+</head>
+<body>
+<h1>Text php </h1>
+
+<?php
+echo "php 01!"
+?>
+
+
+
+
+
+
+
+    <script src="m.js"></script>
+</body>
+</html>
